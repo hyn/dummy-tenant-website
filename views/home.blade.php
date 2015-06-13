8da73ddcd2417630e2831440b8f1563f23fae57b
Daniel Klabbers <dklabbers@hostingxs.nl>
2015-06-13 09:40:32 +0200

    changes to meta

--- views/home.blade.php
+++ views/home.blade.php
@@ -3,17 +3,16 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Laravel - The PHP Framework For Web Artisans</title>
+    <title>Multi tenancy for Laravel by hyn</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="author" content="Taylor Otwell">
-    <meta name="description" content="Laravel - The PHP framework for web artisans.">
-    <meta name="keywords" content="laravel, php, framework, web, artisans, taylor otwell">
+    <meta name="author" content="Daniël `Luceos` Klabbers">
+    <meta name="description" content="Unobtrusive multi tenancy for Laravel; dummy website">
+    <meta name="keywords" content="laravel, php, multi tenant, hyn">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--[if lte IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     <link rel="stylesheet" href="/media/laravel-910df0f0.css">
-    <link rel="apple-touch-icon" href="/favicon.png">
 </head>
 <body class="home">
 
